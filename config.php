b408a724ec47d11a754c7d531c8b13305fa2d59e
footykid3 <carterolson3@gmail.com>
2018-10-31 10:34:29 -0500

    CREATED CONFIG File

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'atr.loras.edu');
+define('DBNAME', 'testDB');
+define('DBUSER', 'lorasAdmin');
+define('DBPASS', 'lorasATR2018');
+define('DBCONNSTRING','mysql:host=atr.loras.edu;dbname=testDB');
+?>
