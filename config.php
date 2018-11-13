5aeccc7bc6e0573f3ce1b7090b890012350ccd28
footykid3 <carterolson3@gmail.com>
2018-11-13 10:28:35 -0600

    Added errors back to Login

--- config.php
+++ config.php
@@ -3,5 +3,7 @@ define('DBHOST', 'atr.loras.edu');
 define('DBNAME', 'testDB');
 define('DBUSER', 'lorasAdmin');
 define('DBPASS', 'lorasATR2018');
-define('DBCONNSTRING','mysql:host=atr.loras.edu;dbname=testDB');
+
+
+
 ?>
