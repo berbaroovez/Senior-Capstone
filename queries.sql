41179ecf0c40640d0027fddf98b1d1bc88c6ba90
shepstream <logan.sheppard11@gmail.com>
2018-11-28 15:39:15 -0600

    added trainerDateLookup query updated trainerDateLookup.php

--- queries.sql
+++ queries.sql
@@ -17,3 +17,12 @@ injuries.Name as 'Injuries Name', injury_report.Description, injury_report.Date
 from injury_report join students on injury_report.studentID = students.ID
 join sports on injury_report.sportID = sports.sportsID
 join injuries on injury_report.InjuryID = injuries.injuriesID;
+
+
+
+query for trainerDateLookup
+select students.FirstName, students.LastName, injuries.Name, injury_report.Description,injury_report.Date
+from injury_report join students on injury_report.studentID = students.ID
+join sports on injury_report.sportID = sports.sportsID
+join injuries on injury_report.InjuryID = injuries.injuriesID
+where injury_report.Date between "2018-11-19" and "2018-11-25";
