eaaa469ebb57995c1af1bcfc3e5667adc4a689c5
footykid3 <carterolson3@gmail.com>
2018-11-28 12:50:21 -0600

    Template

--- php/trainerSportLookup.php
+++ php/trainerSportLookup.php
@@ -1,1 +1,24 @@
-s
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta hhtp-equiv="X-UA-Compatible" content="IE-Edge">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Login Page</title>
+  </head>
+  <body style="background:#D1B888 !important">
+    <div style="background:transparent !important" class ="jumbotron text-center">
+      <div class ="container">
+
+        <h1 class="display-3">Atheletic Trainer Room Sign In</h1>
+
+      </div>
+    </div>
+
+    <!-- Put your stuff here -->
+  </body>
+
+  </html>
