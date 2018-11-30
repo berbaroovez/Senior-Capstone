46ff4688f480b20a598cbe1a62b97bfe0807e861
footykid3 <carterolson3@gmail.com>
2018-11-30 10:06:36 -0600

    dynamically filled table with records

--- php/trainerSportLookup.php
+++ php/trainerSportLookup.php
@@ -1,8 +1,15 @@
-<<?php
+<?php
 
 include("config.php");
 session_start();
-$db = mysqli_connect("localhost", "lorasAdmin", "lorasATR2018", "atr");
+$link = mysqli_connect("localhost", "lorasAdmin", "lorasATR2018", "atr");
+
+
+$sport = $_POST['injury_by_sport'];
+
+
+
+
  ?>
 
 <!DOCTYPE html>
@@ -12,9 +19,9 @@ $db = mysqli_connect("localhost", "lorasAdmin", "lorasATR2018", "atr");
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta hhtp-equiv="X-UA-Compatible" content="IE-Edge">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
-    <title>Login Page</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <title>Sport Page</title>
   </head>
   <body style="background:#D1B888 !important">
     <div style="background:transparent !important" class ="jumbotron text-center">
@@ -25,6 +32,49 @@ $db = mysqli_connect("localhost", "lorasAdmin", "lorasATR2018", "atr");
       </div>
     </div>
 
+    <div>
+      <div class="row">
+        <div class="col-lg-12 text-xs-center">
+          <table class="table">
+            <thead>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Injury</th>
+              <th scope="col">Description</th>
+              <th scope="col">Date</th>
+            </thead>
+            <tbody>
+
+
+              <?php
+
+              $sql = "SELECT students.FirstName, students.LastName,injuries.Name, injury_report.Description, injury_report.Date from injury_report
+              join students on students.ID = injury_report.studentID
+              join injuries on injuries.injuriesID = injury_report.InjuryID
+              where injury_report.sportID = $sport";
+
+              $result = mysqli_query($link,$sql);
+              while ($row=mysqli_fetch_array($result))
+                {
+                echo"<tr>
+                    <td>".$row['FirstName']."</td>
+                    <td>".$row['LastName']."</td>
+                    <td>".$row['Name']."</td>
+                    <td>".$row['Description']."</td>
+                    <td>".$row['Date']."</td>
+                    </tr>
+                 ";
+                }
+
+
+
+               ?>
+            <tbody>
+
+          </table>
+        </div>
+      </div>
+    </div>
     <!-- Put your stuff here -->
   </body>
 
